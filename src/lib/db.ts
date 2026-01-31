f699e7ab929d3543ef367862d420a47a38952e73
sudhanshu khosla <sudhanshukhosla123@gmail.com>
2026-02-01 03:06:48 +0530

    fixed output of prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from "@/generated/prisma/client";
-
+import { PrismaClient } from "@prisma/client";
 
 // Create a singleton Prisma client
 const globalForPrisma = globalThis as unknown as {
@@ -8,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
 
